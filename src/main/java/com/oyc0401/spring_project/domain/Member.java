2f3788060c8804b0efb599f3722ea1f6b1d5607e
oyuchan <oyc0401@gmail.com>
2023-02-14 22:52:15 +0900

    우왕 웹으로 서버 접근 성공했어요

---
+++ src/main/java/com/oyc0401/spring_project/domain/Member.java
@@ -0,0 +1,22 @@
+package com.oyc0401.spring_project.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/oyc0401/spring_project/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.oyc0401.spring_project.service;
+
+import com.oyc0401.spring_project.domain.Member;
+import com.oyc0401.spring_project.repository.MemberRepository;
+import com.oyc0401.spring_project.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return Long
+     */
+    public Long join(Member member) {
+
+        validate(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validate(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
